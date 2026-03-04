d1387a61aa908a123fd2b22e14aa064e86a8a68b
Aya7m <yyoyo6987@gmail.com>
2026-03-04 22:03:07 +0200

    hello

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRouter from './routes/auth.js'
+import messagesRouter from './routes/messages.js'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use("/api/auth", authRouter)
+app.use("/api/messages", messagesRouter)
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
