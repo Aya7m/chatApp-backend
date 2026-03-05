099c11efee951d603724cb1af0449cf2bc13ef30
Aya7m <yyoyo6987@gmail.com>
2026-03-05 22:04:57 +0200

    welcom-email

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/EmailHandler.js";
 import { generateToken } from "../lib/generateToken.js";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+ 
 
 export const signUp = async (req, res) => {
   try {
@@ -27,59 +29,35 @@ export const signUp = async (req, res) => {
     // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // create user
     const NewUser = await User.create({
       fullName,
       email,
       password: hashedPassword,
     });
 
-    if (NewUser) {
-      
-    const saveUser=  await NewUser.save();
-    generateToken(saveUser._id,res)
-      return res
-        .status(201)
-        .json({
-          message: "User created successfully",
-          _id: saveUser._id,
-          fullName: saveUser.fullName,
-          email: saveUser.email,
-          profilePicture: saveUser.profilePicture,
-        });
-        //Todo: send welcome email to user
-    } else {
-      return res.status(400).json({ message: "User not created" });
+    generateToken(NewUser._id, res);
+
+    try {
+      await sendWelcomeEmail(
+        NewUser.email,
+        NewUser.fullName,
+        process.env.CLIENT_URL,
+      );
+    } catch (error) {
+      console.error("Email error:", error);
     }
+
+    return res.status(201).json({
+      message: "User created successfully",
+      _id: NewUser._id,
+      fullName: NewUser.fullName,
+      email: NewUser.email,
+    });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" ,error: error.message});
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
-
 // login
-export const signIn=async(req,res)=>{
-    try {
-        
-        const{email,password}=req.body
-        const user=await User.findOne({email})
-        if(!user){
-            return res.status(400).json({message:"Invalid email or password"})
-        }
-        const isMatch=await bcrypt.compare(password,user.password)
-        if(!isMatch){
-            return res.status(400).json({message:"Invalid email or password"})
-        }
-        generateToken(user._id,res)
-        return res.status(200).json({
-            message:"User logged in successfully",
-            _id:user._id,
-            fullName:user.fullName,
-            email:user.email,
-            profilePicture:user.profilePicture,
-        })
-
-    } catch (error) {
-        return res.status(500).json({ message: "Internal server error", error: error.message });
-    }
-}
