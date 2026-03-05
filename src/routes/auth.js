a94a96f0c71c00bcc526466cf552cac39ee8269b
Aya7m <yyoyo6987@gmail.com>
2026-03-05 12:48:34 +0200

    signUp

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { signUp } from "../controller/auth.controller.js";
 
 const authRouter=Router()
+authRouter.post('/register',signUp)
 
 export default authRouter
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.js'
 import messagesRouter from './routes/messages.js'
+import { connectDB } from './lib/db.js'
 
 dotenv.config()
 const app = express()
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/messages", messagesRouter)
 
+connectDB()
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
