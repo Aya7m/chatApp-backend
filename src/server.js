f1c15feb02105ceffa842acb2bcaadc4002d2f21
Aya7m <yyoyo6987@gmail.com>
2026-03-11 22:16:55 +0200

    cors-edit

--- src/server.js
+++ src/server.js
@@ -10,11 +10,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-}))
+  }),
+);
+app.use(express.json());
+
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messagesRouter);
