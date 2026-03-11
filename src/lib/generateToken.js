c9335628a682c7821c4dff3556ea277643aff0b7
Aya7m <yyoyo6987@gmail.com>
2026-03-11 22:14:41 +0200

    edit-cookies

--- src/lib/generateToken.js
+++ src/lib/generateToken.js
@@ -6,7 +6,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "development" ? false : true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 
