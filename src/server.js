f56eadd80e21a123f034a1d6b8f2925342a11783
Aya7m <yyoyo6987@gmail.com>
2026-03-12 22:46:47 +0200

    بهءءثي

--- src/server.js
+++ src/server.js
@@ -10,13 +10,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  })
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 app.use(express.json());
 app.use(cookieParser());
