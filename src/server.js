cd35f3dbca11b93d69cbc77fd66d6fb9d469c306
Aya7m <yyoyo6987@gmail.com>
2026-03-12 22:12:57 +0200

    editttt

--- src/server.js
+++ src/server.js
@@ -7,17 +7,29 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+// مهم جدا
 app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  origin: ["http://localhost:5173"],
   credentials: true
 }));
 
-app.options("*", cors());
+// السماح بالـ preflight
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials","true");
+  res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
+  
+  if(req.method === "OPTIONS"){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,5 +37,11 @@ app.use("/api/auth", authRouter);
 app.use("/api/messages", messagesRouter);
 
 await connectDB();
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/",(req,res)=>{
+  res.send("Server working");
+});
+
+app.listen(port,()=>{
+  console.log("Server running on port",port);
+});
