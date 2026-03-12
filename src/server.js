2d2c223b0be6c85627ea22af5f39b76004f6332b
Aya7m <yyoyo6987@gmail.com>
2026-03-12 22:05:13 +0200

    يارب

--- src/server.js
+++ src/server.js
@@ -11,12 +11,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  credentials: true
+}));
+
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
